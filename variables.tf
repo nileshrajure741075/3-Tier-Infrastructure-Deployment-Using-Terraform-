4637de6ccd59fcb00f353599598b76e9ecdca44f
Nilesh Rajure <153292119+nileshrajure741075@users.noreply.github.com>
2025-06-28 16:21:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "region" {
   default = "us-east-1"
   }
 variable "ami_id" {
-  default = "ami-0731b************"
+  default = "ami-0731becbf832f281e"
 }
 variable "ins_type" {
   default = "t2.micro"
