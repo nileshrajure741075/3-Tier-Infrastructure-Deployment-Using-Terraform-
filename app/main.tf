3b2cf8e94f6b986de600aa834d5cd58c2cc4323d
Nilesh Rajure <153292119+nileshrajure741075@users.noreply.github.com>
2025-06-28 16:16:12 +0530

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,10 +1,10 @@
 # app-instance
-resource "aws_instance" "appec2" {
+resource "aws_instance" "app2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
-  key_name = "tf-key-pair"
+  key_name = "infra-key"
 tags = {
     Name = "app-instance"
 }
